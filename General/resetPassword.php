82c086d195721df15c6b876acb74ba0de7b69a06
laeffy7403 <laeffy7403@gmail.com>
2024-05-12 01:02:56 +0800

    2.7

--- General/resetPassword.php
+++ General/resetPassword.php
@@ -1,3 +1,8 @@
+<?php 
+        session_start();
+        include('../Sys/connection.php');   
+?>
+
 <!DOCTYPE html>
 
 <html>
@@ -25,11 +30,15 @@
 
 
         <?php 
+          
+        $loginType = $_SESSION['loginType']; 
+
         //using GET method and POST method together
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $id = strtoupper(trim($_POST["id"]));
             $password = trim($_POST["pass"]);
             $passwordCfn = trim($_POST["passCfn"]);
+
             // Validate the new password format
             $errorA = validatePass($password);
             $errorB = validateCfnPass($passwordCfn, $password);
@@ -39,28 +48,45 @@
                 echo "<div class='error'>$errorA</div>";
             } else if(!empty($errorB)) {
                 echo "<div class='error'>$errorB</div>";
+            } else {
+
+            $con = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+            
+            // if($_SESSION['id'] == 'admin') {
+            //     $sql = "UPDATE admin SET admin_pass = ? WHERE admin_id = ?";
+            // } else if($_SESSION['id'] == 'member') {
+            //     $sql = "UPDATE member SET member_pass = ? WHERE member_id = ?";
+            // } else {
+            //     //ERROR MESSAGE
+            //     echo "<div class='error'>Invalid user role.</div>";
+            //     exit;
+            // }
+
+        if ($loginType == 'member') {
+            $sql = "UPDATE member SET member_pass = ? WHERE member_ id = ?";
+        }
+        else if ($loginType == 'admin') {
+            $sql = "UPDATE admin SET admin_pass = ? WHERE admin_id = ?";
+        }
+        else {
+            echo "<div class='error'>Invalid login type.</div>";
+            exit;
+        }
+
+            $stmt = $con->prepare($sql);
+            $stmt->bind_param("ss", $password, $id);
+            $stmt->execute();
+
+            if ($stmt->affected_rows > 0) {
+                // Password reset successful
+                echo "<div class='info'>Password reset successfully for ID: $id.</div>";
+            } else {
+                // Unable to update password
+                echo "<div class='error'>Unable to reset password. Please try again.</div>";
             }
-             else {
-                // Update the password in the database
-                $con = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-        
-                $sql = "UPDATE admin SET admin_pass = ? WHERE admin_id = ?";
-                //NOTE: query() no "?" in sql
-                //NOTE: prepare() "?" in sql 
-                $stmt = $con->prepare($sql);
-                $stmt->bind_param("ss", $password, $id);
-                $stmt->execute();
-        
-                if ($stmt->affected_rows > 0) {
-                    // Password reset successful
-                    echo "<div class='info'>Password reset successfully for Admin ID: $id.</div>";
-                } else {
-                    // Unable to update password
-                    echo "<div class='error'>Unable to reset password. Please try again.</div>";
-                }
-        
-                $stmt->close();
-                $con->close();
+            
+            $stmt->close();
+            $con->close();
             }
         }
         ?>
@@ -69,28 +95,28 @@
 
         <div class = 'input_box'>
                         <label class="input">
-                            <input class = "input_field" type = "text" name  = "id" value=""/>
+                            <input class = "input_field" type = "text" name  = "id" value="" placeholder=""/>
                             <span class="input_label">Id</span>
                         </label>
                     </div>
         
                     <div class = 'input_box'>
                         <label class="input">
-                            <input class = "input_field" type = "text" name  = "pass" value=""/>
+                            <input class = "input_field" type = "text" name  = "pass" value="" placeholder=""/>
                             <span class="input_label">Password</span>
                         </label>
                     </div>
 
                     <div class = 'input_box'>
                         <label class="input">
-                            <input class = "input_field" type="text" name="passCfn"  value=""/>  
+                            <input class = "input_field" type="text" name="passCfn"  value="" placeholder=""/>  
                             <span class="input_label">Password Confirmation</span>
                         </label>
                     </div>
 
             <br/>
             <input type="submit" value="Update" id="btnUpdate" name="btnUpdate" />
-            <input type="button" value="Cancel" id="btnCancel" name="Cancel" onclick="location='adminList.php'" />
+            <input type="button" value="Cancel" id="btnCancel" name="Cancel" onclick="location='../Admin/menuAdmin.php'" />
             </form>
             <br/>
 
